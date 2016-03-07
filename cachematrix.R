27cd231e76c63a2f1d6847cd3a0110fd51402f0d
Goran Cetusic <goran.cetusic@gmail.com>
2016-03-07 19:24:43 +0100

    implement cachable matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Take matrix as argument or generate empty matrix and return list
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Take cachable matrix list and calculate and store matrix inverse
+cacheinverse <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting inverse data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
